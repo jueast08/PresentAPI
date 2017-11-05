e8920e6dab1c2a51762b80ea35623047c6ec15ce
Julian EASTERLY <julianeasterly@gmail.com>
2017-11-05 10:44:50 +0100

    config(server): replaced Jersey with Jersey2, updated maven dependecies, and deleted obsolete web.xml file

---
+++ src/fr/presentapi/config/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package fr.presentapi.config;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class ApplicationConfig extends Application {
+	@Override
+	public Map<String, Object> getProperties() {
+		Map<String, Object> properties = new HashMap<String, Object>();
+		properties.put("jersey.config.server.provider.packages", "fr.presentapi.rest");
+		return properties;
+	}
+}
