40e2a8fc637bbbbd3b20952fdb3e44690ea325eb
Coline Smagghe <smagghe@ecole.ensicaen.fr>
2017-11-10 17:33:56 +0100

    DbConnection(created)

---
+++ src/fr/presentapi/dao/DbConnection.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.presentapi.dao;
+
+/**
+ *
+ * @author Coline
+ */
+public class DbConnection {
+    /* ce qu'il faut faire exactement : 
+    class static, singleton(ne peut etre instancié une seule fois 
+    get_connecte qui renvoie l'objet existant ou qui le creer grace au cosntucteur privé
+    constructeur privé 
+    _conn
+    voir shema
+    */
+}
